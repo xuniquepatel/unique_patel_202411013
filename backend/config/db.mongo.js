6a719fb36905106f883537a83806941ab845d27e
xuniquepatel <patelunique1@gmail.com>
2025-10-24 03:41:22 +0530

    server-side

--- backend/config/db.mongo.js
+++ backend/config/db.mongo.js
@@ -1,19 +1,17 @@
 import { MongoClient } from "mongodb";
 import { MONGO_URL } from "./env.js";
-
 let client, db, products;
 
 export async function initMongo() {
-  client = new MongoClient(MONGO_URL);
+  client = new MongoClient(MONGO_URL, { serverSelectionTimeoutMS: 15000 });
   await client.connect();
-  db = client.db("ecomdb");
+  db = client.db();
   products = db.collection("products");
   await products.createIndex({ sku: 1 }, { unique: true });
   await products.createIndex({ category: 1 });
   await products.createIndex({ updatedAt: -1 });
   return { db, products };
 }
-
 export function getProducts() {
   return products;
 }
